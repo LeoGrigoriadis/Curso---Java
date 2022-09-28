c18552c0703e2467b35401d16dfa9843763385a2
LeoGrigoriadis <leogrigo2014@gmail.com>
2022-09-28 15:47:07 -0300

    clase-01

---
+++ Curso-Java/src/main/java/Clase_01/actividad_1/App.java
@@ -0,0 +1,17 @@
+package Clase_01.actividad_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a=12,  b=34;
+    	System.out.println("*****************************");
+    	System.out.println(" Probando un nuevo proyecto.");
+    	System.out.println("*****************************\n");
+        System.out.println( b+" - "+a+" = "+(b - a) );
+    }
+}
